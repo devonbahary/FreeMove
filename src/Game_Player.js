4265660784c86b6f5993ac1364922508676ea4f9
devonbahary <devonbahary@gmail.com>
2018-11-11 11:00:54 -0800

    adds 8-dir for Game_Player

--- src/Game_Player.js
+++ src/Game_Player.js
@@ -4,3 +4,8 @@
 // The game object class for the player. It contains event starting
 // determinants and map scrolling functions.
 
+
+// overwritten
+Game_Player.prototype.getInputDirection = function() {
+  return Input.dir8;
+};
