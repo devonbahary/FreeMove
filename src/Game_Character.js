86aeca0236bba0272e81c6bddc84ce83c2dc3a60
devonbahary <devonbahary@gmail.com>
2018-11-23 00:12:01 -0800

    adds movement functions for Game_Character

--- src/Game_Character.js
+++ src/Game_Character.js
@@ -11,6 +11,32 @@ Game_Character.prototype.updateAutoMove = function(dx, dy) {
   }
 };
 
+Game_Character.prototype.moveRandom = function() {
+  let dir = 5;
+  while (dir === 5) dir = Math.ceil(Math.random() * 9);
+
+  const dx = this.isLeft(dir) ? -1 : this.isRight(dir) ? 1 : 0;
+  const dy = this.isUp(dir) ? -1 : this.isDown(dir) ? 1 : 0;
+  this.autoMove(dx, dy);
+};
+
+Game_Character.prototype.moveTowardCharacter = function(character) {
+  let dx = this.dxFrom(character);
+  let dy = this.dyFrom(character);
+  dx = Math.sign(dx) * Math.min(1, Math.abs(dx));
+  dy = Math.sign(dy) * Math.min(1, Math.abs(dy));
+  this.autoMove(dx, dy);
+};
+
+Game_Character.prototype.moveAwayFromCharacter = function(character) {
+  this.autoMove(Math.sign(this.x - character.x), Math.sign(this.y - character.y));
+};
+
+Game_Character.prototype.moveTo = function(x, y) {
+  if (!$gameMap.isValid(x, y)) return;
+  this.autoMove(x - this.x, y - this.y);
+};
+
 Game_Character.prototype.dxFrom = function(char) {
   const cof = Math.sign(char.x - this.x);
   const distance = Math.max(0, Math.abs(char.x - this.x) - (char.hitboxRadius() + this.hitboxRadius()));
